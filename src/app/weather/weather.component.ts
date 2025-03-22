8f6655bc1e6bffd72499ec02f07f72f5d9eb2c20
Diogo Goulart <136640390+DiogoGoularttt@users.noreply.github.com>
2025-03-22 11:22:13 -0300

    :sparkles: feat: Add configurations

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { environment } from '../../environments/environment';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { isPlatformBrowser } from '@angular/common';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -18,29 +19,37 @@ export class WeatherComponent {
   weatherData: any;
   faMagnifyingGlass = faMagnifyingGlass;
 
-  constructor(private http: HttpClient) {}
-
+  constructor(
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
   ngOnInit() {
     this.getUserLocation();
   }
 
   getUserLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const lat = position.coords.latitude;
-          const lon = position.coords.longitude;
-          this.getWeatherByCoords(lat, lon);
-        },
-        (error) => {
-          console.error('Erro ao obter localização:', error);
-          alert(
-            'Não foi possível obter sua localização. Por favor, habilite a permissão de localização.'
-          );
+    if (isPlatformBrowser(this.platformId)) {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+            this.getWeatherByCoords(lat, lon);
+          },
+          (error) => {
+            console.error('Erro ao obter localização:', error);
+            if (isPlatformBrowser(this.platformId)) {
+              alert(
+                'Não foi possível obter sua localização. Por favor, habilite a permissão de localização.'
+              );
+            }
+          }
+        );
+      } else {
+        if (isPlatformBrowser(this.platformId)) {
+          alert('Geolocalização não é suportada pelo seu navegador.');
         }
-      );
-    } else {
-      alert('Geolocalização não é suportada pelo seu navegador.');
+      }
     }
   }
 
@@ -55,7 +64,7 @@ export class WeatherComponent {
 
   searchWeather() {
     const apiKey = '45f8d6a48ac2279c8954243037bb47b8';
-    const geocodingUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${this.cityName}&appid=${apiKey}`;
+    const geocodingUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${this.cityName}&appid=${apiKey}`;
 
     this.http.get(geocodingUrl).subscribe((geocodingResponse: any) => {
       if (geocodingResponse.length > 0) {
